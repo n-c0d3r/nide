b7adac0965cc9403d6dab85d876d535d8e193de1
n-c0d3r <dntechunity@gmail.com>
2021-11-29 11:07:48 +0700

    fix bug.

--- src/nide.js
+++ src/nide.js
@@ -12,6 +12,11 @@ class Nide{
 
         this.cursor = 0;
 
+
+        this.selectedBIndex=-1;
+        this.selectedEIndex=-1;
+
+
         this.console = new Console();
 
         this.console.keypressEventListeners['Nide'] = function(ch,key){
@@ -39,16 +44,27 @@ class Nide{
                 }
 
                 if(key && key.name == "left"){
-                    app.cursor = clamp(app.cursor-1,0,app.code.length);
-
-                    app.ReprintCode();
+                    app.Left();
                     return;
                 }
 
                 if(key && key.name == "right"){
-                    app.cursor = clamp(app.cursor+1,0,app.code.length);
+                    app.Right();
+                    return;
+                }
 
-                    app.ReprintCode();
+                if(key && key.name == "up"){
+                    
+                    return;
+                }
+
+                if(key && key.name == "down"){
+                    
+                    return;
+                }
+
+                if(key && key.name == "tab"){
+                    app.AddCode('    ');
                     return;
                 }
 
@@ -61,11 +77,14 @@ class Nide{
                     app.AddCode('\n');
                     return;
                 }
-                if(key.shift){
-                    app.AddCode(key.name.toUpperCase());
-                }
-                else{
-                    app.AddCode(key.name);
+
+                if(!key.ctrl && !key.meta){
+                    if(key.shift){
+                        app.AddCode(key.name.toUpperCase());
+                    }
+                    else{
+                        app.AddCode(key.name);
+                    }
                 }
             }
             else{
@@ -108,17 +127,38 @@ class Nide{
         
         let compiledCode = this.CompileCode(this.code);
 
-        let func = Function(compiledCode)();
+        try{
+            let func = Function(compiledCode)();
+            
+            this.code = '';
+    
+            console.clear();
+            
+            process.stdout.write('\x1b[36mRun:\x1b[37m\n');
+    
+            return (func(this));
+        }
+        catch(err){
+            console.error(err);
+        }
 
-        process.stdout.write('\n');
+    }
 
-        return (func(this));
+    Left(){
+        this.cursor = clamp(this.cursor-1,0,this.code.length);
 
+        this.ReprintCode();
+    }
+
+    Right(){
+        this.cursor = clamp(this.cursor+1,0,this.code.length);
+
+        this.ReprintCode();
     }
 
     AddCode(key){
         this.code = this.code.substring(0,this.cursor) + key + this.code.substring(this.cursor,this.code.length);
-        this.cursor = clamp(this.cursor+1,0,this.code.length);
+        this.cursor = clamp(this.cursor+key.length,0,this.code.length);
 
         this.ReprintCode();
     }
@@ -141,17 +181,21 @@ class Nide{
         this.ReprintCode();
     }
 
-    ReprintCode(){
+    ReprintCode(option){
         console.clear();
         this.console.HideCursor();
 
-        // \x1b[46m
         let coloredCode = this.code;
 
-        if(this.cursor<this.code.length)
-            coloredCode = this.code.substring(0,this.cursor) + '\x1b[46m' + this.code[this.cursor] + '\x1b[40m' + this.code.substring(this.cursor+1,this.code.length);
-        else{
-            coloredCode = this.code.substring(0,this.code.length) + '\x1b[46m' + ' ' + '\x1b[40m';
+        if(option==null)
+            option=new Object();
+
+        if(!(option.printColoredCode == true)){
+            if(this.cursor<this.code.length)
+                coloredCode = this.code.substring(0,this.cursor) + '\x1b[46m' + this.code[this.cursor] + '\x1b[40m' + this.code.substring(this.cursor+1,this.code.length);
+            else{
+                coloredCode = this.code.substring(0,this.code.length) + '\x1b[46m' + ' ' + '\x1b[40m';
+            }
         }
 
         process.stdout.write(coloredCode);
